2d761ea7f6a4e31040887aa7802874b5b054698b
= <=>
2021-09-16 14:23:20 -0500

    init

---
+++ ConnectToAirpods/Program.cs
@@ -0,0 +1,25 @@
+﻿using InTheHand.Net.Sockets;
+using System;
+using System.Linq;
+
+namespace ConnectToAirpods
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            using var bluetoothClient = new BluetoothClient();
+
+            var devices = bluetoothClient.DiscoverDevices();
+
+            var airPodsPro = devices.FirstOrDefault(device => device.DeviceName == "AirPods Pro");
+
+            if (airPodsPro == null || airPodsPro.Connected)
+            {
+                Environment.Exit(-1);
+            }
+
+            //bluetoothClient.Connect(airPodsPro.DeviceAddress, )
+        }
+    }
+}
